d449177e09fe16a0cb54aea1343c626e6e6bc753
unknown <girishchauhan333@gmail.com>
2024-02-25 22:05:57 +0530

    fix: JavaScript Code

--- main.js
+++ main.js
@@ -34,8 +34,6 @@ function selectOption(){
         if(dropDownEl.value === 'cm'){
             cemtiDivEl.style.display = ''; 
             feetDivEl.style.display = 'none';
-
-            
             heightFeetEl.value = '';
             heightInchesEl.value = '';
             heightCentimeterEl.value = '';
@@ -45,8 +43,6 @@ function selectOption(){
 
             feetDivEl.style.display = '';
             cemtiDivEl.style.display = 'none';
-
-            
             heightFeetEl.value = '';
             heightInchesEl.value = '';
             heightCentimeterEl.value = '';     
@@ -56,7 +52,6 @@ function selectOption(){
             cemtiDivEl.style.display = 'none';
             optEml.style.display = 'none';
             optMsgEml.style.display = 'none';
-
             weightElm.value ='';
             heightFeetEl.value = '';
             heightInchesEl.value = '';
@@ -67,11 +62,9 @@ function selectOption(){
     
     function calculateBMI(){
 
-            var kg = weightElm.value;
-
-            var cmValue = heightCentimeterEl.value;
-
-            var feetValue = heightFeetEl.value;
+            var kg = parseFloat(weightElm.value);
+            var cmValue = parseFloat(heightCentimeterEl.value);
+            var feetValue = parseFloat(heightFeetEl.value);
             var inchesValue = heightInchesEl.value;
 
             optEml.style.display = '';
@@ -81,6 +74,7 @@ function selectOption(){
 
             var bmiValueFeet;
 
+
             if(dropDownEl.value === 'cm'){
                 
             // Centimeter Calculation
@@ -88,19 +82,19 @@ function selectOption(){
             var meterSqrCenti = meterCenti * meterCenti;
             bmiValueCentimeter = (kg / meterSqrCenti).toFixed(1);
             
-                if(kg === '' || kg == 0 || cmValue == '' || cmValue == 0 || bmiValueCentimeter === 'Infinity' || bmiValueCentimeter === 'NaN'){
-                    //optEml.innerHTML = `BMI = 0`;
+        
+                if( bmiValueCentimeter === 'Infinity' || bmiValueCentimeter === 'NaN' || cmValue === '' || cmValue === 0 || kg === 0 && typeof(cmValue) === 'number' || kg === '' && typeof(cmValue) === 'number'){
+                    optEml.style.display = 'none';
                     optMsgEml.innerHTML = `Please enter a valid input.`;
-                }
-                else if(bmiValueCentimeter > 0 && bmiValueCentimeter <= 18.4){
+                } else if(bmiValueCentimeter > 0 && bmiValueCentimeter <= 18.4){
                     optEml.innerHTML = `BMI = ${bmiValueCentimeter}`;
                     optMsgEml.innerHTML = `Underweight`;
 
-                } else if(bmiValueCentimeter == 18.5 || bmiValueCentimeter <= 24.9){
+                } else if(bmiValueCentimeter <= 18.5 || bmiValueCentimeter <= 24.9){
                     optEml.innerHTML = `BMI = ${bmiValueCentimeter}`;
                     optMsgEml.innerHTML = `Normal`;
 
-                } else if(bmiValueCentimeter == 25.0 || bmiValueCentimeter <= 39.9){
+                } else if(bmiValueCentimeter <= 25.0 || bmiValueCentimeter <= 39.9){
                     optEml.innerHTML = `BMI = ${bmiValueCentimeter}`;
                     optMsgEml.innerHTML = `Overweight`;
 
@@ -118,19 +112,19 @@ function selectOption(){
                 var meterSqrFeet = meterFeet * meterFeet;
                 bmiValueFeet = (kg / meterSqrFeet).toFixed(1);
 
-                if(kg === '' || kg == 0 || feetValue == '' || feetValue == 0|| bmiValueFeet === 'Infinity' || bmiValueFeet === 'NaN'){
-                    //optEml.innerHTML = `BMI = 0`;
+                if( bmiValueFeet === 'Infinity' || bmiValueFeet === 'NaN' || feetValue === '' || feetValue === 0|| kg === 0 && typeof(feetValue) === 'number' || kg === '' && typeof(feetValue) === 'number'){
+                    optEml.style.display = 'none';
                     optMsgEml.innerHTML = `Please enter a valid input.`;
 
                 } else if(bmiValueFeet > 0 && bmiValueFeet <= 18.4){
                     optEml.innerHTML = `BMI = ${bmiValueFeet}`;
                     optMsgEml.innerHTML = `Underweight`;
 
-                } else if(bmiValueFeet == 18.5 || bmiValueFeet <= 24.9){
+                } else if(bmiValueFeet <= 18.5 || bmiValueFeet <= 24.9){
                     optEml.innerHTML = `BMI = ${bmiValueFeet}`;
                     optMsgEml.innerHTML = `Normal`;
 
-                } else if(bmiValueFeet == 25.0 || bmiValueFeet <= 39.9){
+                } else if(bmiValueFeet <= 25.0 || bmiValueFeet <= 39.9){
                     optEml.innerHTML = `BMI = ${bmiValueFeet}`;
                     optMsgEml.innerHTML = `Overweight`;
 
